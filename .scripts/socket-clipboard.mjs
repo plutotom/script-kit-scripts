dfd1e02cf5ab8c37ece49a71bcab621e7e01bef5
Isaiah-Allarium <Isaiah.proctor@allarium.com>
2023-02-05 20:29:06 -0600

    pushed from Script Kit

--- .scripts/socket-clipboard.mjs
+++ .scripts/socket-clipboard.mjs
@@ -1,4 +1,4 @@
-// .kenv/kenvs/plutotom/scripts/socket-clipboard.ts
+// Users/plutotom/.kenv/kenvs/plutotom/scripts/socket-clipboard.ts
 import "@johnlindquist/kit";
 import * as http from "http";
 var express = await npm("express");
@@ -7,24 +7,15 @@ var clipboardListener = await npm("clipboard-event");
 var { io: ioClient } = await npm(
   "socket.io-client"
 );
-var SERVER_TYPE = await env(
-  "SOCKET_SERVER_TYPE",
-  async () => await arg("select Type", ["CLIENT" /* CLIENT */, "SERVER" /* SERVER */])
-);
 var MY_PORT = await env("MY_PORT", "My port the server should run on?");
 var SERVER_PORT = await env(
   "SERVER_PORT",
   "What is the port of the server you need to link to?"
 );
-var PC_ADDRESS = await env("PC_IP");
-if (SERVER_TYPE === "SERVER") {
-  dev("Starting server");
-} else if (SERVER_TYPE === "CLIENT") {
-  dev("Starting client");
-} else if (SERVER_TYPE === "CLIENT" /* CLIENT */) {
-  dev("Starting client number 2");
-  await env("SOCKET_SERVER_TYPE", "");
-}
+var SERVER_IP = await env(
+  "SERVER_IP",
+  "this is the ip of the other computer you want to connect to"
+);
 var app = express();
 var server = http.createServer(app);
 var io = new Server(server);
@@ -32,13 +23,16 @@ clipboardListener.startListening();
 clipboardListener.on("change", async () => {
   let [latest] = await getClipboardHistory();
   let text = await paste();
-  await io.emit("TO_WINDOWS", latest || text);
+  await io.emit("TO_SERVER_EVENT", latest || text);
 });
-io.on("connection", (socket) => {
+io.on("connection", async (socket) => {
   console.log("a user connected");
+  await dev("a user connected");
+  await dev(socket);
 });
-var socketClient = await ioClient(`http://${PC_ADDRESS}:${SERVER_PORT}`, {});
-await socketClient.on("MAC_EVENT", async (clipboardRes) => {
+var socketClient = await ioClient(`http://${SERVER_IP}:${SERVER_PORT}`, {});
+await socketClient.on("TO_SERVER_EVENT", async (clipboardRes) => {
+  await dev(clipboardRes.text || clipboardRes.value);
   await kit.log("recieved clipboard from server", clipboardRes.value);
 });
 app.get("/testing", (req, res) => {
--- .scripts/test-cron-job.mjs
+++ .scripts/test-cron-job.mjs
@@ -1,3 +1,5 @@
 // Users/plutotom/.kenv/kenvs/plutotom/scripts/test-cron-job.ts
 import "@johnlindquist/kit";
-var res = await $`nmap`;
+var res = await $`/opt/homebrew/bin/nmap -sV -p 3000-3010 10.0.0/24 | grep -E "Nmap scan report for|PORT|open|MAC Address"`;
+await dev(res.stdout);
+await hide();
