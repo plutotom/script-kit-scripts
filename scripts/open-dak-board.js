dd3bf79ef269b7813e094dc4d70d734af0fe6375
isaiah Proctor <IsaiahD.Proctor@gmail.com>
2023-04-22 23:42:18 -0500

    pushed from Script Kit

---
+++ scripts/open-dak-board.js
@@ -0,0 +1,7 @@
+// Name: open Dak Board
+// This is simply a shrotcut to open Dak Board in your default browser
+
+import "@johnlindquist/kit";
+
+const dakBoardUrl = "https://dakboard.com/app";
+await browse(dakBoardUrl);
