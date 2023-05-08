29322bc40a5242e2c316e2b343e21ccf3d8ccb2c
Isaiah-Allarium <Isaiah.proctor@allarium.com>
2023-05-07 21:05:06 -0500

    pushed from Script Kit

--- scripts/open-coding-project.ts
+++ scripts/open-coding-project.ts
@@ -4,20 +4,14 @@
 import "@johnlindquist/kit";
 
 // list all folders in ~/Documents/coding
-// let codingFolder = await ls("~/Documents/coding");
 let basePath = await env("CODING_FOLDER_LOCATION", async () => {
-  // let basePath = await arg("Coding Folder Location?", async () => {
-  //   let basePath = await ls("~/Documents/coding");
-  //   return basePath;
-  // });
   return selectFolder("Coding Folder Location?");
-  // return readdir(basePath);
 });
 
 let codingFolder = await readdir(basePath);
 //sort codingFolder alphabetically
 codingFolder.sort();
-// adds "Make New Folder" to the top of the list
+// Adds "Make New Folder" to the top of the list
 codingFolder.unshift("Make New Folder");
 
 let selectedFolder = await arg(
@@ -30,14 +24,9 @@ let selectedFolder = await arg(
 );
 if (selectedFolder.includes("Make New Folder")) {
   let folderName = await arg("Folder Name?");
-  // Give new prompt asking for folder name, on return create new folder, cd into it, and open termnal at it
+  // Give new prompt asking for folder name, on return create new folder, cd into it, and open terminal at it
   await $`mkdir ${basePath}/${folderName}`;
-  // await $`open -a Terminal ${basePath}${folderName}`;
   await $`code ${basePath}/${folderName}`;
-  // await edit(`${basePath}/${folderName}`);
 } else {
-  // await $`code ${selectedFolder}`;
   await edit(selectedFolder);
 }
-
-// await $`open -a Terminal ${selectedFolder}`;
