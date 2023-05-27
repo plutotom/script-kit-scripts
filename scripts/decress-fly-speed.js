fdc788126f4e6f611b3f4e7ebd6c81104f1e20d6
isaiah <isaiahd.proctor@gmail.com>
2023-05-27 09:53:51 -0500

    pushed from Script Kit

--- scripts/decress-fly-speed.js
+++ scripts/decress-fly-speed.js
@@ -1,9 +1,9 @@
-// Name: incress fly speed
-// Snippet: 333
+// Name: decress fly speed
+// Snippet: 222
 import "@johnlindquist/kit";
 
 // on press of the "f" key, type /fs 10
 await keyboard.pressKey(Key.Slash);
 await wait(100);
-await keyboard.type("fs 10");
+await keyboard.type("fs 2");
 await keyboard.pressKey(Key.Enter);
