6d4dbd5a4f7285f6d173b10403f7145d887e8305
isaiah <isaiahd.proctor@gmail.com>
2023-06-02 17:14:24 -0500

    pushed from Script Kit

--- scripts/open-app-from-desktop.ts
+++ scripts/open-app-from-desktop.ts
@@ -1,4 +1,27 @@
 // Name: open app from desktop
 
-import "@johnlindquist/kit"
+import "@johnlindquist/kit";
 
+let desktopPath = await env("DESKTOP_PATH", async () => {
+  return selectFolder("Enter your Desktop Location");
+});
+// returns an array with all the files/folders names.
+let desktopFolder = await readdir(desktopPath);
+// desktopFolder.sort();
+
+// sort out .ini files
+desktopFolder.filter((el, i, arr) => {
+  return !el.includes(".ini");
+});
+
+let selectedShortcut = await arg(
+  "Pick a Shortcut",
+  desktopFolder.map((folder) => ({
+    name: folder,
+    description: home(desktopPath, folder),
+    value: home(desktopPath, folder),
+  }))
+);
+
+// click selectedShortcut
+await open(selectedShortcut);
