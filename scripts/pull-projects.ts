4097fbe3aaae163e730594847b038c66fb8e22a9
Isaiah-Allarium <Isaiah.proctor@allarium.com>
2023-06-10 18:37:47 -0500

    pushed from Script Kit

--- scripts/pull-projects.ts
+++ scripts/pull-projects.ts
@@ -28,3 +28,56 @@ await codingFolder.forEach(async (el, inx, arr) => {
 
 // await wait(5000);
 // await exit();
+
+// Description: Git Pull Kenv Repo
+import { getKenvs } from "../core/utils.js";
+let kenvs = (await getKenvs()).map((value) => ({
+  name: path.basename(value),
+  value,
+}));
+kenvs.unshift({
+  name: "main",
+  value: kenvPath(),
+});
+let dir = await arg("Pull which kenv", kenvs);
+cd(dir);
+await term({
+  command: `git fetch`,
+  preview: md(`# Pulling a Kenv
+
+> The terminal only ran "git fetch" to show you what changes are available.
+
+You still need to run "get merge" to apply the changes.
+  
+  `),
+  cwd: dir,
+  shortcuts: [
+    {
+      name: "Stash",
+      key: `${cmd}+s`,
+      bar: "right",
+      onPress: async () => {
+        term.write(`git stash`);
+      },
+    },
+    {
+      name: "Merge",
+      key: `${cmd}+m`,
+      bar: "right",
+      onPress: async () => {
+        term.write(`git merge`);
+      },
+    },
+    {
+      name: "Exit",
+      key: `${cmd}+w`,
+      bar: "right",
+      onPress: async () => {
+        submit("");
+      },
+    },
+  ],
+});
+await getScripts(false);
+await mainScript();
+//# sourceMappingURL=kenv-pull.js.map
