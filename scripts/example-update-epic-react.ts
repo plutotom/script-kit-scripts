9c289c9829607f1413529fd71f22c192700be4f0
isaiah <isaiahd.proctor@gmail.com>
2023-06-11 08:29:41 -0500

    pushed from Script Kit

---
+++ scripts/example-update-epic-react.ts
@@ -0,0 +1,25 @@
+// Menu: Update EpicReact deps (Example)
+// Description: Update all the dependencies in the epicreact workshop repos
+
+const repos = [
+  "advanced-react-hooks",
+  "advanced-react-patterns",
+  "bookshelf",
+  "react-fundamentals",
+  "react-hooks",
+  "react-performance",
+  "react-suspense",
+  "testing-react-apps",
+];
+
+const script = `git add -A && git stash && git checkout main && git pull && ./scripts/update-deps && git commit -am 'update all deps' --no-verify && git push && git status`;
+for (const repo of repos) {
+  const scriptString = JSON.stringify(
+    `cd ~/code/epic-react/${repo} && ${script}`
+  );
+  //   exec(
+  //     `osascript -e 'tell application "Terminal" to activate' -e 'tell application "Terminal" to do script ${scriptString}'`
+  //   );
+  // or
+  terminal(scriptString); // (I don't need to await this because I don't care about waiting)
+}
