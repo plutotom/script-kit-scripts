0c0ab5f36806218da04cccac2144e5235d3c98a2
Isaiah-Allarium <Isaiah.proctor@allarium.com>
2023-07-13 20:36:36 -0500

    updatin

--- scripts/clone-git-repo.ts
+++ scripts/clone-git-repo.ts
@@ -3,29 +3,29 @@
 // Twitter: @orhan_erday
 
 import "@johnlindquist/kit";
+const { Octokit } = await npm("octokit");
+
 // const projectLocation = await env(
 //   "PROJECT_LOCATION",
 //   "Enter project location (defaults to home dir), i.e. /Documents/Coding"
 // );
 
-var w = await widget(
-  `<div style="margin:10px;">
-  
-      <h1 style="color: black;">Set your project location </h1>
-      <p style="color: black;">i.e. /Documents/Coding</p>
-    </div>
-    `,
-  {
-    width: 300,
-    height: 75,
-  }
-);
+// var w = await widget(
+//   `<div style="margin:10px;">
+
+//       <h1 style="color: black;">Set your project location </h1>
+//       <p style="color: black;">i.e. /Documents/Coding</p>
+//     </div>
+//     `,
+//   {
+//     width: 300,
+//     height: 75,
+//   }
+// );
 
 let projectLocation = await env("PROJECT_LOCATION", async () => {
   return selectFolder("Coding Folder Location?");
 });
-
-const { Octokit } = await npm("octokit");
 
 interface RawRepositoryType {
   id: string;
