ef5fd65f7a37cce5e31dad456729536dbceadeba
Isaiah Proctor <proctoi@onecollective.org>
2023-10-17 12:36:33 -0500

    push plutotom

---
+++ scripts/php-pint-run.ts
@@ -0,0 +1,16 @@
+// Name: Php pint formate this file
+// Snippet: ppht
+
+import "@johnlindquist/kit";
+
+// await keyboard.pressKey(Key.LeftControl);
+// await keyboard.pressKey(Key.Grave);
+
+// await setTimeout(() => {
+//   return;
+// }, 1000);
+await keyboard.type("./vendor/bin/pint --dirty");
+// await setTimeout(() => {
+//   return;
+// }, 1000);
+// await keyboard.pressKey(Key.Enter);
